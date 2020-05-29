a5566d2aeebfbb83b5b38ac80612cd83c82e0593
Pedro Mac <vaz.valois@df.ufscar.br>
2020-05-29 00:46:36 +0000

    tests created and travis.yml configured

---
+++ tests.py
@@ -0,0 +1,12 @@
+# -----------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See LICENSE in the project root for license information.
+# -----------------------------------------------------------------------------------------
+
+def add(x: int, y: int) -> int:
+    return x + y
+
+
+def test_add():
+    assert add(10, 11) == 21
+    assert add(5, -5) == 0
